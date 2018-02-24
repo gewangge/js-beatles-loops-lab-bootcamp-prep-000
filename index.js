154c2a799fb30ed0d9fe52d2de1ca9651bb0e3a1
Ge Wang <wg.wangge1984@gmail.com>
2018-02-24 14:17:10 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,16 @@
+function theBeatlesPlay(musicians, instruments) {
+  var array = []
+  for (var i = 0; i < musicians.length; i++) {
+      array.push(musicians[i] + " plays " + instruments[i])
+  }
+  return array
+}
+
+function johnLennonFacts(array) {
+  var i = 0
+  while ( i < array.length) {
+    array[i] + " !!!"
+    array++
+  }
+  return array
+}
